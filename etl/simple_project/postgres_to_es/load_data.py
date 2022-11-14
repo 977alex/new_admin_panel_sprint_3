ccdc51e0d663d3ae69be087a9947ac1e32a0b931
977alex <info@97799.ru>
2022-11-14 21:41:26 +0300

    доработки экспорта в эластик v7

--- etl/simple_project/postgres_to_es/load_data.py
+++ etl/simple_project/postgres_to_es/load_data.py
@@ -29,28 +29,19 @@ def etl(
     start_timestamp = datetime.datetime.now()
     filmwork_ids = state.get_state("filmwork_ids")
 
-    # извлекаем данные для переноса их из PostgreSQL в Elasticsearch
     for extracted_part in extracrot.extract(
         last_sync_timestamp, start_timestamp, filmwork_ids
     ):
-        # преобразовываем данные в формат, удобоваримый Elasticsearch
         data = transformer.transform(extracted_part)
-        # грузим в Elasticsearch
         loader.load(data)
-        # фиксируем время синхронизации
         state.set_state("last_sync_timestamp", str(start_timestamp))
-        # обнуляем список filmwork_ids
         state.set_state("filmwork_ids", [])
 
 
 if __name__ == "__main__":
-    # определяем основные настройки
     configs = BaseConfig()
-    # создаем логгер
     logger = get_logger(__name__)
-    # начинаем храненить состояние, чтобы при перезапуске приложения продолжить работу с места остановки, а не начинать процесс заново
     state = State(JsonFileStorage(file_path="state.json"))
-    # подготавливаем экземпляры классов для работы ETL (Extract-Transform-Load процесс перекачки данных из PostgreSQL в Elasticsearch)
     extractor = Extractor(
         psql_dsn=configs.dsn,
         chunk_size=configs.chunk_size,
@@ -59,7 +50,6 @@ if __name__ == "__main__":
     )
     transformer = Transformer()
     loader = Loader(dsn=configs.es_base_url, logger=logger)
-    # запускаем процесс ETL (Extract-Transform-Load процесс перекачки данных из PostgreSQL в Elasticsearch)
     while True:
         etl(logger, extractor, transformer, state, loader)
         logger.info(f"сон на {configs.sleep_time}")
